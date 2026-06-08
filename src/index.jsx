ed54c7d51fec3645b50b85dcf308191eb60976b9
ranggautama47 <ranggautama737@gmail.com>
2026-06-08 08:50:38 +0700

    feat: add darkmode.css and import in index

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import App from './components/App.jsx';
 
 // import style
 import './styles/style.css';
+import './styles/darkmode.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
