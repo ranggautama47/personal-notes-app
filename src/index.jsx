be48dbdb4060cf393f74434577429d152a6538b7
ranggautama47 <ranggautama737@gmail.com>
2026-06-08 13:54:09 +0700

    chore: update entry point untuk Phase 2

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
 // import style
+import './styles/variables.css';
 import './styles/style.css';
 import './styles/darkmode.css';
 
